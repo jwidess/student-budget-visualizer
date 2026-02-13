e19b79ba501485aa12eafab121a47f0d6254b5aa
Justin Widen <jdogwiden5@gmail.com>
2026-02-13 00:23:24 -0600

    Fix date value fitment

--- src/components/inputs/IncomeForm.tsx
+++ src/components/inputs/IncomeForm.tsx
@@ -142,7 +142,7 @@ export function IncomeForm() {
                     }
                     min={minDate}
                     max={maxDate}
-                    className="w-full rounded-md border border-input bg-background px-3 py-1.5 text-sm focus:outline-none focus:ring-2 focus:ring-ring"
+                    className="w-full rounded-md border border-input bg-background px-1 py-1.5 text-sm focus:outline-none focus:ring-2 focus:ring-ring"
                   />
                 </div>
               </div>
